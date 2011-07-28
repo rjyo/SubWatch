dd08ae2f60bd6cd71b9cbe33e9350193e35a731c
Rakuraku Jyo <jyo.rakuraku@gmail.com>
2011-07-28 10:13:28 +0900

    Initial Commit

---
+++ SubWatch/SubWatchAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  SubWatchAppDelegate.h
+//  SubWatch
+//
+//  Created by 徐 楽楽 on 11/07/28.
+//  Copyright 2011年 ラクラクテクノロジーズ. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SubWatchAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
