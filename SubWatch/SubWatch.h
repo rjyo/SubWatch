91bd98b8cf991a991a4a04649b231a1fa1d6c02f
James Chen <ashchan@gmail.com>
2011-07-29 17:28:57 +0900

    looks like we need a PreferencePanes project

---
+++ SubWatch/SubWatch.h
@@ -0,0 +1,15 @@
+//
+//  SubWatch.h
+//  SubWatch
+//
+//  Created by James Chen on 7/29/11.
+//  Copyright 2011年 ラクラクテクノロジーズ. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface SubWatch : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ SubWatch/SubWatch.m
@@ -0,0 +1,17 @@
+//
+//  SubWatch.m
+//  SubWatch
+//
+//  Created by James Chen on 7/29/11.
+//  Copyright 2011年 ラクラクテクノロジーズ. All rights reserved.
+//
+
+#import "SubWatch.h"
+
+@implementation SubWatch
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
